4591fe3a7072f41fe4174e438a162981c53df137
badboy420444-blip <badboy420444@gmail.com>
2025-07-22 19:18:42 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lucky~fRlmzBAQ#wpFI4EHJXm2eRVTj72rJLZ900fUm_hyHSGFOzX5t2zY",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "93702730615",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "",
